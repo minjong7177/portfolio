77a3855172f7d035348c58cef9cc9e4857b00a3d
minjong7177 <minjong7177@gmail.com>
2026-04-20 20:18:39 +0900

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,12 @@
+fetch("/api/profile")
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById("job").innerText = data.job;
 
+        const skills = document.getElementById("skills");
+        data.skills.forEach(skill => {
+            const li = document.createElement("li");
+            li.innerText = skill;
+            skills.appendChild(li);
+        });
+    });
