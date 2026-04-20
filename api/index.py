b3075e4476d24488ed047e1c5545a468b3ab7183
minjong7177 <minjong7177@gmail.com>
2026-04-20 20:17:09 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,21 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return "Flask API is running!"
+
+@app.route("/api/profile")
+def profile():
+    return jsonify({
+        "name": "JAY",
+        "job": "Software Developer",
+        "skills": ["Python", "Flask", "JavaScript"]
+    })
+
+# Vercel용 handler
+def handler(request, response):
+    return app(request.environ, response.start_response)
